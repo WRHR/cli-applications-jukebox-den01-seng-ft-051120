89d2ed80b84884d68a79d42e55c5daeb2b76d972
Will Reeves <wrhreeves@gmail.com>
2020-04-14 23:05:47 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -18,7 +18,10 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.strip
-  if user_input == ("1" || "2")
+  if (1..9).include?(user_input.to_i)
+    puts "Playing #{songs[user_input.to_i - 1]}"
+  elsif songs.include?()
+  end
 end
 
 def exit_jukebox
