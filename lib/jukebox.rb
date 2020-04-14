7a99c4c6110b5439b458bdcffd3bdaa16e6a5302
Will Reeves <wrhreeves@gmail.com>
2020-04-14 22:51:06 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,4 +1,12 @@
 # Add your code here
 def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help messagge"
+  puts "- list : displays a list of songs your can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list 
   
 end
